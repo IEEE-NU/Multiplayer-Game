5bf6056ac9c9673f6252838828aab2eaa0d6d439
LuoLei_Zhao <LuoLei_Zhao>
2017-01-30 15:08:08 -0600

    Added multiplayer, Splitscreen and lever/Geyser object

---
+++ Assets/Script/Geyser.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic; //REMEMBER! In order to use lists! Make sure it is System.Collections.Generic instead of System.Collections
+
+public class Geyser : MonoBehaviour {
+
+	public string geyserID = "default";
+	List<Controller2D> overlappingControl = new List<Controller2D> (); 
+	public float geyserTime = 0.0f;
+	public float geyserSpeed = -60.0f;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		if (geyserTime > 0.0f) {
+			Vector3 upForce = new Vector3 (0, geyserSpeed, 0);
+			foreach(Controller2D cont in overlappingControl) {
+				Debug.Log (upForce);
+				cont.Move (upForce);
+			}
+			geyserTime -= Time.deltaTime;
+		}
+	}
+	public void activateGeyser(float time) {
+		Debug.Log (time);
+		geyserTime = time;
+	}
+	internal void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("collision detected with Geyser");
+		overlappingControl.Add (other.gameObject.GetComponent<Controller2D> ());
+	}
+	internal void OnTriggerExit2D(Collider2D other) {
+		Debug.Log ("Collision ended with Geyser");
+		overlappingControl.Remove (other.gameObject.GetComponent<Controller2D> ());
+	}
+}
---
+++ Assets/Script/Lever.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Lever : MonoBehaviour {
+
+	public string collideThisPlayer = "Player 1";
+	public string geyserID = "default";
+	List<Player> overlappingPlayer = new List<Player> (); 
+
+	// Use this for initialization
+	void Start () { }
+
+	// Update is called once per frame
+	void Update () {
+		foreach(Player play in overlappingPlayer) {
+			if (play.attemptingInteraction == true) {
+				activateLever (play);
+			}
+		}
+	}
+
+	internal void activateLever(Player interactor) {
+		Debug.Log ("Activating Lever");
+		Geyser[] geysers = GameObject.FindObjectsOfType<Geyser> ();
+		foreach (Geyser geyser in geysers) {
+			if (geyser.geyserID == geyserID) {
+				geyser.activateGeyser(3.0f);
+			}
+		}
+	}
+
+	internal void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.GetComponent<Player> ()) {
+			Debug.Log ("Player Added");
+			overlappingPlayer.Add (other.gameObject.GetComponent<Player> ());
+		}
+	}
+	internal void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.GetComponent<Player> ()) {
+			Debug.Log ("Player Removed");
+			overlappingPlayer.Remove (other.gameObject.GetComponent<Player> ());
+		}
+	}
+}
