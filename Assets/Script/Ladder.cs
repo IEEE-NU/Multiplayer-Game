d4b920ec6721323eb60f6669ec7398137a1c040d
LuoLei_Zhao <luoleizhao2018@u.northwestern.edu>
2017-03-03 17:07:09 -0600

    Added a background Manager SCript

--- Assets/Script/Ladder.cs
+++ Assets/Script/Ladder.cs
@@ -7,7 +7,7 @@ public class Ladder : MonoBehaviour {
     public string collideThisPlayer = "Player 1";
     public string geyserID = "default";
     List<Player> overlappingPlayer = new List<Player>();
-
+	public float maxVertical = 40;
     // Use this for initialization
     void Start() { }
 
@@ -18,6 +18,7 @@ public class Ladder : MonoBehaviour {
         {
             if (Input.GetButtonDown(play.gameObject.GetComponent<Player>().upKey))
             {
+				Debug.Log ("Ladder upping");
                 activateLadder(play);
             }
         }
@@ -25,8 +26,11 @@ public class Ladder : MonoBehaviour {
 
     internal void activateLadder(Player interactor)
     {
+		Controller2D cont = interactor.GetComponent<Controller2D> ();
         Debug.Log("Activating Ladder");
-
+		if (cont.velocity.y < maxVertical) {
+			cont.addToVelocity (new Vector2(0,-40f)); // adds a force on the object.
+		}
     }
 
     internal void OnTriggerEnter2D(Collider2D other)
