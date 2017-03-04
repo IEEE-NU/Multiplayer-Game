4b5be6288c6032e0dad74beb40074273a923c2d0
LuoLei_Zhao <luoleizhao2018@u.northwestern.edu>
2017-03-03 20:55:19 -0600

    Added player specific platforms

---
+++ Assets/Script/PlayerSpecific.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpecific : MonoBehaviour {
+
+	BoxCollider2D mCollider;
+	public string collideThisPlayer = "Player 2";
+	// Use this for initialization
+	void Start () {
+		mCollider = GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	internal void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag (collideThisPlayer)) {
+			mCollider.isTrigger = false;
+		} else {
+			mCollider.isTrigger = true;
+		}
+	}
+}
